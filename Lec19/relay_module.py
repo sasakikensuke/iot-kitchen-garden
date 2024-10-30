ec8b636cd8530ec2bfc841f4ecddd6db775dec61
Kensuke Sasaki <52873849+sasakikensuke@users.noreply.github.com>
2024-10-30 14:38:41 +0900

    Update relay_module.py

--- Lec19/relay_module.py
+++ Lec19/relay_module.py
@@ -1,5 +1,5 @@
 from time import sleep
-from gpiozero import OutputDevice
+import RPi.GPIO as GPIO
 import argparse
 import schedule
 
@@ -10,14 +10,28 @@ DEFAULT_INTERVAL_TIME = 10
 
 class Scheduler(object):
     def __init__(self, channel, interval):
-        self._relay = OutputDevice(channel)
+        self._channel = channel
         self._interval = interval
 
+    def setup(self):
+        GPIO.setwarnings(False)
+        GPIO.setmode(GPIO.BOARD)
+        GPIO.setup(self._channel, GPIO.OUT)
+
     def power_on_and_off(self):
-        self._relay.on()
+        self.setup()
+        GPIO.output(self._channel, 0)
         sleep(self._interval)
-        self._relay.off()
+        self.cleanup()
         sleep(self._interval)
+
+    def power_off(self):
+        GPIO.output(self._channel, 1)
+
+    @staticmethod
+    def cleanup():
+        GPIO.cleanup()
+
 
 def main():
     parser = argparse.ArgumentParser(description="Relay Module Script")
@@ -33,25 +47,29 @@ def main():
         "--wait-time",
         type=int,
         default=DEFAULT_WAIT_TIME,
-        help="set power on wait seconds (default {} seconds)".format(DEFAULT_WAIT_TIME),
+        help="set power on wait seconds (default {} seconds)".format(
+            DEFAULT_WAIT_TIME),
     )
     parser.add_argument(
         "-i",
         "--interval",
         type=int,
         default=DEFAULT_INTERVAL_TIME,
-        help="set script interval seconds (default {} seconds)".format(DEFAULT_INTERVAL_TIME),
+        help="set script interval seconds (default {} seconds)".format(
+            DEFAULT_INTERVAL_TIME
+        ),
     )
     args = parser.parse_args()
 
     scheduler = Scheduler(args.channel, args.wait_time)
     schedule.every(args.interval).seconds.do(scheduler.power_on_and_off)
-    
+
     try:
         while True:
             schedule.run_pending()
             sleep(1)
     except KeyboardInterrupt:
+        scheduler.cleanup()
         pass
 
 
